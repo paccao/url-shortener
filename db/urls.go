5ce31153d5f3a3eba296ce1701392a33ecd808bc
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2023-11-12 22:32:28 +0100

    Added decode json file func
    Added func to decode json file, handle err and return []URL or an error

--- db/urls.go
+++ db/urls.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 )
@@ -19,4 +20,24 @@ func GetUrl(url string) {
 	}
 	defer jsonFile.Close()
 	fmt.Printf("Successfully read %s\n", fileName)
+
+	urls, decodeErr := decodeJsonFile(jsonFile)
+	if decodeErr != nil {
+		fmt.Println("Unexpected error while decoding json file")
+		os.Exit(0)
+	}
+
+	// TODO: find url in urls
+
+}
+
+func decodeJsonFile(file *os.File) ([]URL, error) {
+	var urls []URL
+	decoder := json.NewDecoder(file)
+	decodeJsonErr := decoder.Decode(&urls)
+	if decodeJsonErr != nil {
+		fmt.Println("Error decoding JSON: ", decodeJsonErr)
+		return nil, errors.New("error decoding JSON from file")
+	}
+	return urls, nil
 }
